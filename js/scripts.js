3bf0055b8a9b1f9e97404e9b5a9fe0d03ef2562e
Abel Trotter and Nikki Boyd <abel+nikki@>
2018-06-04 10:05:23 -0700

    display array items as list item when user submits page. remove list items when users submits again

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
+function getData() {
+  var favStuff = [];
+  $("input").each(function(index, item) {
+    favStuff.push($(item).val());
+  });
+  console.log(favStuff);
+  return favStuff;
+}
+
+function displayResult(array) {
+  for(i = 0; i < array.length; i++) {
+    $("#allResults").append("<li class='listItem'>" + array[i] + "</li>");
+  }
+}
+
 $(document).ready(function() {
   $("#favoriteThings").submit(function(event) {
-    
     event.preventDefault();
+    $(".listItem").remove();
+    var results = getData();
+    displayResult(results);
   });
 });
